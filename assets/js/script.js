f2586eb6640829145288a590c0cf2ea00a65f321
Aman Aujla <aujla7736@gmail.com>
2020-10-29 21:38:24 -0500

    my code added in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,103 +1,76 @@
-$(document).ready(function () {
-
-
-  var todayDate;
-  var cityName = [];
-  var image;
-  var maxDistance;
-
-
-  function getWeather() {
-    var city = document.getElementById("search").value;
-    var units = document.getElementById("units").value;
-    fetch("http://api.weatherstack.com/current?access_key=e063281f4936be92105758e9da02670d&query=" + city
-      + "&units=" + units)
-      .then(a => a.json())
-      .then(response => {
-        console.log('======================WEATHER RESPONSE======================\n', response , '=========================================\n')
-        
-        document.getElementById("image").src = response.current.weather_icons[0];
-        document.getElementById("output").innerHTML = "<h3>" + response.location.name + "</h3>Temperature: " + response.current.temperature + "F" + "<br><hr>Feels like: " + response.current.feelslike + "<br><hr>UV index: " + response.current.uv_index + "<br><hr>Humidity: " + response.current.humidity + "<br><hr>Description: " + response.current.weather_descriptions;
-        return {lat: response.location.lat , lon: response.location.lon}
-      }).then(coords => {
-        console.log('----------------coords-------------------------', coords)
-        var lat = coords.lat;
-        var lon = coords.lon;
-
-
-
-        $.ajax({
-          url: "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=10&key=200952288-580b87b672e00ea3e6f7ec1e05ad0bb9",
-          method: "GET"
-        }).then(function (response) {
-          console.log( '\n=================TRAILS==============\n', response.trails, '\n===================================\n')
-          var trails = response.trails
-             
-          $(trails).each(function(index, el) {
-            console.log( `${index}----> `, el)
-
-             var labelData = {
-                      ascent: 0,
-                      conditionDate: "",
-                      conditionDetails: null,
-                      conditionStatus: "",
-                      descent: 0,
-                      difficulty: "",
-                      high: 0,
-                      id: 0,
-                      imgMedium: "",
-                      imgSmall: "",
-                      imgSmallMed: "",
-                      imgSqSmall: "",
-                      latitude: 0,
-                      length: 0,
-                      location: "",
-                      longitude: 0,
-                      low: 0,
-                      name: "",
-                      starVotes: 0,
-                      stars: 0,
-                      summary: "",
-                      type: "",
-                      url: ""
-            }
-          
-            var parent = $('#cardDisplay').attr("style", "width: 30rem");
-            var imageSm  = $('<img>').attr('src', el.imgSqSmall, "style", "width: 300px;", "height: 300px;", "margin: 5px");
-            var name = $('<h6>').text('' + el.name).attr("style", "margin-bottom: 2px");
-            var difficult = $('<p>').text('Difficult:' + el.difficulty);
-            var summary = $('<p>').text('' + el.summary).attr("style", "margin: 3px");
-
-
-
-
-            parent.append(imageSm, name, difficult, summary)
-            $("#cardDisplay").append(parent);
-
-          })
-          
-         
-           })
-      })
+var todayDate;
+var cityName = [];
+var image;
+var maxDistance;
+function displayCityName(event) {
+  var cityName = $("#zipcode").val()
+  console.log(cityName)
+// display the trails
+const API_KEY ="8b267c19ed20a20ec0f113d0fe760b1b"
+ const url=`https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&zip=${cityName}`
+// const url= "https://api.weatherstack.com/current"
+$.ajax({
+  url: url,
+  // data: {
+  //   access_key: 'e063281f4936be92105758e9da02670d',
+  //   query: cityName
+  // },
+  dataType: 'json',
+  success: function (apiResponse) {
+    console.log(apiResponse);
+    // const lat= apiResponse.location.lat
+    // const lon= apiResponse.location.lon
+    const lat= apiResponse.coord.lat
+    const lon= apiResponse.coord.lon
+    trailData(lat,lon)
+    }
+});
 }
-
-  $("#weatherbutton").on("click", getWeather);
-
-
-  //hiking API Key
+$("#search").on("click",displayCityName)
+//async function currentWeather(city){
+//const url= `"https://api.weatherstack.com/current&units=m=609c6ca41a9803f4a3b46553fc8f5c5c"`
+//return await $.ajax({
+//url: url, 
+//method: "GET"
+//});
+//}
+// display the trails
+//function renderDayCard (){ 
+//$('#cards').empty();
+//var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + locationName + "&units=imperial&appid=ed2ee741f61d9d60983426ca204e9ed6";
+//$.ajax ({
+//url: queryURL,
+//method: "GET"
+//}).then(function (response){
+//for(var i = 0; i < response.list.length; i++){
+//if (response.list[i].dt_txt[12] === "5") {
+//console.log(response.list[i].dt_txt);
+//var card = $('<section>').addClass("card text-black bg-primary text-white").attr("style", "width: 7rem;");
+//var cardBody = $('<div>').addClass("card-body");
+//var cardDay = $('<h4>').addClass("card-title").text(moment.unix(response.list[i].dt).format("MM/DD/YYYY"));
+//var cardTemp = $('<p>').addClass('card-text').text("Temp:" + response.list[i].main.temp + "F");
+//var cardHumidity = $('<p>').addClass("card-text").text("Humidity: " + response.list[i].main.humidity + "%");
+//var cardImg = $('<img>').attr("src","http://openweathermap.org/img/w/" + image + ".png");
+//cardBody.append(cardDay, cardImg, cardTemp, cardHumidity);
+//card.append(cardBody);
+//$("#cards").append(card);
+//}
+//}
+//})
+//}
+//weather API key
+//hiking API Key
 function trailData(lat,lon) {
-  
-
+  // var lat = 40.0274;
+  // var lon = -105.2519;
   $.ajax({
-   
+    //url:"https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200952288-580b87b672e00ea3e6f7ec1e05ad0bb9",
     url: "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=10&key=200952288-580b87b672e00ea3e6f7ec1e05ad0bb9",
     method: "GET"
   }).then(function (response) {
     console.log(response)
 const trails= response.trails.map(function(trail){
-
-
-const trailTemplate = `
+    const trailTemplate = `
 <div class="card">
 <div class="card-image">
   <img src="${trail.imgSqSmall}" width="200" height="200">
@@ -117,4 +90,12 @@ return trailTemplate;
 $("#trail-cards").html(trails.join(""))
   })
 }
-})
+// trailData();
+//enter zip code (saves zip to local storage) append to a button to select previous location
+// displays weather (Location & Day)
+// displays 6 cards (closets)
+// color codes the trail ( "green" shortest distance, "yellow" < 7 mile hike, advanced > more than 7 mile hike (red))
+// get elevation and apply to the trails
+//$("#button").on("click", async function(event){
+  //var userinput= $("input").val()
+  //const currentWeatherResponse= await currentWeather(userinput)
